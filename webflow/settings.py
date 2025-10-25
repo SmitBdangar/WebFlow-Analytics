7dbe67e9027641a5cef74edb3971f17ae0f239b5
Smit Dangar <smityadav011@gmail.com>
2025-10-25 13:25:57 +0530

    launch

--- webflow/settings.py
+++ webflow/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g47^iqw@v1cq8u-(*0663je7skz=(cbzf-z)r)%0$rby-e#^y^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
